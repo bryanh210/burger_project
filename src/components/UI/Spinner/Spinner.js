6ac34a00c33f07d8dc7b563537886b03c76e50d4
bryanh210 <bryantuonghoang@gmail.com>
2020-11-09 13:29:24 -0500

    update spinner

--- src/components/UI/Spinner/Spinner.js
+++ src/components/UI/Spinner/Spinner.js
@@ -2,8 +2,9 @@ import React from 'react';
 import classes from './Spinner.css';
 
 const spinner = () => {
+    // if the css isn't shown, then the text is shown
     return (
-        <div className={classes.Spiner} />
+        <div className={classes.Spinner}>Loading...</div>
     )
 }
 
