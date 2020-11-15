8b17757301838ce93f861ff40a65b1ec529418b6
bryanh210 <bryantuonghoang@gmail.com>
2020-11-15 18:55:34 -0500

    finish improving performance

--- src/App.js
+++ src/App.js
@@ -4,22 +4,12 @@ import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
-  state = {
-    show: true
-  };
-
-  // just for testing
-  componentDidMount() {
-	  setTimeout(() => {
-		  this.setState({show: false})
-	  }, 1000)
-  }
 
   render () {
     return (
       <div>
         <Layout>
-          {this.state.show ? <BurgerBuilder /> : null}
+          <BurgerBuilder />
         </Layout>
       </div>
     );
